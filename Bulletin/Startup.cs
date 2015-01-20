bc12d570e1c7e360374cf51d90e484272894b20d
AndreaSoto <andreasoto@gmail.com>
2015-01-20 11:29:51 -0700

    For the Win!

---
+++ Bulletin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bulletin.Startup))]
+namespace Bulletin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
